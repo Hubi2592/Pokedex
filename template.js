9b34b6bb5ad734aa4783b046f9576503afa6ffc8
Hubi2592 <133772535+Hubi2592@users.noreply.github.com>
2026-06-13 16:30:25 +0200

    OverlaySyling&Functions

--- template.js
+++ template.js
@@ -18,41 +18,42 @@ function pokemonCardTemplate(pokemon) {
 function overlayTemplate(pokemon) {
     const mainType = pokemon.types[0].type.name;
     return `
-        <div class="overlayContent"
-        style="background-color: ${typeColorsBackground[mainType]};">
-            <button id="closeOverlay" onclick="closeOverlay()">✕</button>
+        <div class="overlayContent" style="background-color: ${typeColorsBackground[mainType]};">
+            <button id="closeOverlay" onclick="closeOverlay()">&times;</button>
             <h2>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
             <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}">
             <div class="pokemonTypes">${pokemon.types.map(type => `<span class="type ${type.type.name}" style="background-color: ${typeBadgeColors[type.type.name]};">
                 ${type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}</span>`).join('')}
             </div>
             <div class="overlayTabs">
-                <button id="aboutButton" onclick="showTab('about', ${pokemon.id})">About</button>
-                <button onclick="showTab('stats', ${pokemon.id})">Base Stats</button>
-                <button onclick="showTab('evolution', ${pokemon.id})">Evolution</button>
-                <button onclick="showTab('moves', ${pokemon.id})">Moves</button>
+                <button class="overlayTab activeTab" id="aboutButton" onclick="showTab('about', ${pokemon.id}, this)">About</button>
+                <button class="overlayTab" id="statsButton" onclick="showTab('stats', ${pokemon.id}, this)">Base Stats</button>
+                <button class="overlayTab" id="evolutionButton" onclick="showTab('evolution', ${pokemon.id}, this)">Evolution</button>
+                <button class="overlayTab" id="movesButton" onclick="showTab('moves', ${pokemon.id}, this)">Moves</button>
             </div>
             <div id="tabContent" class="pokemonInfo">
-            ${aboutTemplate(pokemon)}
+                ${aboutTemplate(pokemon)}
             </div>
-            <button id="previousPokemon" onclick="previousPokemon()">←</button>
-            <button id="nextPokemon" onclick="nextPokemon()">→</button>
+            <div class="navigationButtons">
+                <button id="previousPokemon" onclick="previousPokemon()"><img class="arrowLeft" src="./assets/icons/right-arrow.png" alt="Previous"></button>
+                <button id="nextPokemon" onclick="nextPokemon()"><img class="arrowRight" src="./assets/icons/right-arrow.png" alt="Next"></button>
+            </div>
         </div>
     `;
 }
 
 async function aboutTemplate(pokemon) {
-         const species = await fetchJson(pokemon.species.url);
-      return `
-        <div>
-            <p><b>ID:</b> #${pokemon.id}</p>
-            <p><b>Height:</b> ${pokemon.height}</p>
-            <p><b>Weight:</b> ${pokemon.weight}</p>
-            <p><b>Abilities:</b> ${pokemon.abilities.map(a => a.ability.name).join(", ")}</p>
+    const species = await fetchJson(pokemon.species.url);
+    return `
+        <div class="aboutContainer">
+            <div class="aboutRow"><span class="aboutLabel">ID</span><span class="aboutValue">#${pokemon.id}</span></div>
+            <div class="aboutRow"><span class="aboutLabel">Height</span><span class="aboutValue">${(pokemon.height/10).toFixed(1)} m</span></div>
+            <div class="aboutRow"><span class="aboutLabel">Weight</span><span class="aboutValue">${(pokemon.weight/10).toFixed(1)} kg</span></div>
+            <div class="aboutRow"><span class="aboutLabel">Abilities</span><span class="aboutValue">${pokemon.abilities.map(a => a.ability.name).join(", ")}</span></div>
             <h3>Breeding</h3>
-            <p><b>Gender:</b> ${getGenderText(species.gender_rate)}</p>
-            <p><b>Egg Groups:</b> ${species.egg_groups.map(group => group.name).join(", ")}</p>
-            <p><b>Egg Cycle:</b> ${species.hatch_counter}</p>
+            <div class="aboutRow"><span class="aboutLabel">Gender</span><span class="aboutValue">${getGenderText(species.gender_rate)}</span></div>
+            <div class="aboutRow"><span class="aboutLabel">Egg Groups</span><span class="aboutValue">${species.egg_groups.map(g => g.name).join(", ")}</span></div>
+            <div class="aboutRow"><span class="aboutLabel">Egg Cycle</span><span class="aboutValue">${species.hatch_counter}</span></div>
         </div>
     `;
 }
